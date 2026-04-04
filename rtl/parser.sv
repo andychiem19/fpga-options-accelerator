91f589b8d7c2c5b73852455fb75dbc6840f8c6e0
andychiem19 <andy.chiem19@gmail.com>
2026-04-03 23:31:45 -0400

    parser v1

--- rtl/parser.sv
+++ rtl/parser.sv
@@ -1,6 +1,8 @@
 module parser (
     input   logic clk,
-    input   logic rst_n,
+    input   logic nrst,
+
+    // AXI-Stream signals
     input   logic [7:0] tdata,
     input   logic tvalid,
     input   logic tlast,
@@ -9,6 +11,28 @@ module parser (
     output  logic packet_valid,
     output  logic packet_end
 );
+
+
+/* ETHERNET FRAME STRUCTURE 
+
+ERROR
+-->
+IDLE:
+Preamble: 7 bytes of 0x55
+SFD (Start Frame Delimiter): 1 byte of 0xD5
+
+READ_ETH
+Destination MAC: 6 bytes
+Source MAC: 6 bytes
+EtherType: 2 bytes
+
+IP Header: 20 bytes, or IHL*4           // READ_IP
+UDP Header: 8 bytes                     // READ_UDP
+Payload: Data being transmitted         // PAYLOAD
+
+tlast -> assert packet_end and transition back to IDLE
+
+*/
 
 typedef enum logic [2:0] {
     IDLE,
@@ -20,8 +44,10 @@ typedef enum logic [2:0] {
 } parser_state;
 
 // add necessary registers here
-logic [2:0] preamble_count;
+logic [2:0] preamble_count; 
 logic [3:0] eth_count;
+logic [5:0] ip_count; // IHL's max value is 15, so max IP header size is 15 * 4 = 60 bytes; but usually just 20 bytes
+logic [2:0] udp_count; // 8 bytes
 
 typedef struct packed {
     logic [15:0] ethertype;
@@ -33,11 +59,18 @@ parser_state state;
 protocol_t protocol;
 
 // main parser FSM
-always_ff @(posedge clk or negedge rst_n) begin
-    if (!rst_n) begin
+always_ff @(posedge clk or negedge nrst) begin
+    if (!nrst) begin
         state <= IDLE;
+
+        // Initialize registers
         preamble_count <= 0;
-        eth_count <= 0
+        eth_count <= 0;
+        ip_count <= 0;
+        udp_count <= 0;
+
+        // Default for IPv4
+        protocol.ipbytes <= 20;
     end 
     
     else begin
@@ -71,7 +104,7 @@ always_ff @(posedge clk or negedge rst_n) begin
                 if (tvalid) begin
                     eth_count <= eth_count + 1;
 
-                    if (eth_count == 12) begin // Reads ethertype bytes for next state info
+                    if (eth_count == 12) begin // Reads Ethertype
                         protocol.ethertype[15:8] <= tdata;
                     end
 
@@ -79,29 +112,63 @@ always_ff @(posedge clk or negedge rst_n) begin
                         protocol.ethertype[7:0] <= tdata;
                         eth_count <= 0;
 
-                        if (protocol.ethertype == 16'h0800) begin // IPv4 protocol
-                            protocol.ipbytes <= 20;
-                        end else begin
-                            protocol.ipbytes <= 0;
-                        end
-
-                        state <= READ_IP;
+                        if (protocol.ethertype[15:8] == 8'h08 && tdata == 8'h00) // IPv4, needs to check against current tdata because bottom half is assigned in the same cycle
+                            state <= READ_IP;
+                        else
+                            state <= ERROR; // unrecognized protocol
                     end
                 end
             end
 
             READ_IP: begin
+                if (tvalid) begin
+                    ip_count <= ip_count + 1;
+
+                    if (ip_count == 0) begin
+                        protocol.ipbytes <= (tdata[3:0] * 4); 
+                    end
+
+                    if (ip_count == protocol.ipbytes - 1) begin
+                        ip_count <= 0;
+                        state <= READ_UDP;
+                    end
+                end
             end
 
             READ_UDP: begin
+                if (tvalid) begin
+                    udp_count <= udp_count + 1;
+
+                    if (udp_count == 7) begin
+                        udp_count <= 0;
+                        state <= PAYLOAD;
+                    end
+                end
             end
 
             PAYLOAD: begin
+                if (tvalid) begin
+                    packet <= tdata;
+                    if (tlast) begin
+                        packet_end <= 1;
+                        packet_valid <= 0;
+                        state <= IDLE;
+                    end else begin
+                        packet_valid <= 1;
+                        packet_end <= 0;
+                    end
+                end else begin
+                    packet_valid <= 0;
+                    packet_end <= 0;
+                end
             end
 
             ERROR: begin
                 // resets FSM and registers when an error state is reached
                 state <= IDLE;
             end
+        endcase
     end
 end
+
+endmodule
